376164b3684bc834fd70f971c055b2d6a4ab1d6f
anbuyle2 <an.buyle@gmail.com>
2020-09-29 21:04:44 +0200

    First commit of C++ Hello world

---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+/*
+ * main.cpp
+ *
+ *  Created on: 29 Sep 2020
+ *      Author: anb
+ */
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
+
+
